b879982f911346dbc37c4f1ea84593d91c84b455
jpdark007 <jpdark007@users.noreply.github.com>
2016-02-16 14:43:01 +0100

    Refactor bean CFONB 160

--- src/main/java/org/bankio/bean/BeanCfonb160.java
+++ src/main/java/org/bankio/bean/BeanCfonb160.java
@@ -9,23 +9,23 @@ public class BeanCfonb160{
     private List<BeanCfonb160Recipient> recipients = new ArrayList<BeanCfonb160Recipient>();
     private BeanCfonb160Total total;
 
-    public void setEmetteur(BeanCfonb160Sender emetteur) {
-        this.sender = emetteur;
+    public void setSender(BeanCfonb160Sender sender) {
+        this.sender = sender;
     }
 
-    public void setDestinataires(List<BeanCfonb160Recipient> destinataires) {
-        this.recipients = destinataires;
+    public void setRecipients(List<BeanCfonb160Recipient> recipients) {
+        this.recipients = recipients;
     }
 
     public void setTotal(BeanCfonb160Total total) {
         this.total = total;
     }
 
-    public BeanCfonb160Sender getEmetteur() {
+    public BeanCfonb160Sender getSender() {
         return sender;
     }
 
-    public List<BeanCfonb160Recipient> getDestinataires() {
+    public List<BeanCfonb160Recipient> getRecipients() {
         return recipients;
     }
 
