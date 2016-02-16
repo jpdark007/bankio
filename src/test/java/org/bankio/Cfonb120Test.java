812fff68e366319dfc6ea9727c4076cc8b3857d6
jpdark007 <jpdark007@users.noreply.github.com>
2016-02-16 14:24:09 +0100

    Fix cfonb120 test issues

--- src/test/java/org/bankio/Cfonb120Test.java
+++ src/test/java/org/bankio/Cfonb120Test.java
@@ -5,20 +5,20 @@ import java.util.List;
 
 import org.junit.BeforeClass;
 import org.junit.Test;
+import static org.junit.Assert.*;
 
-import junit.framework.TestCase;
 import main.java.org.bankio.bean.BeanCfonb120;
 import main.java.org.bankio.bean.BeanCfonb120Additional;
 import main.java.org.bankio.bean.BeanCfonb120Movement;
 import main.java.org.bankio.bean.BeanCfonb120NewAmount;
 import main.java.org.bankio.bean.BeanCfonb120OldAmount;
 
-public class Cfonb120Test extends TestCase {
+public class Cfonb120Test {
 	
-	private BeanCfonb120 beanCfonb120;
+	private static BeanCfonb120 beanCfonb120;
 	
 	@BeforeClass
-	private void onceExecutedBeforeAll() {
+	public static  void onceExecutedBeforeAll() {
 		BeanCfonb120OldAmount beanCfonb120OldAmount = new BeanCfonb120OldAmount();
 		beanCfonb120OldAmount.setTypeEnregistrementCode("01");
 		
@@ -30,19 +30,20 @@ public class Cfonb120Test extends TestCase {
 		BeanCfonb120NewAmount beanCfonb120NewAmount = new BeanCfonb120NewAmount();
 		beanCfonb120NewAmount.setTypeEnregistrementCode("07");
 		
+		beanCfonb120 = new BeanCfonb120();
 		beanCfonb120.setOldAmount(beanCfonb120OldAmount);
 		beanCfonb120.setMovements(beanCfonb120Movements);
 		beanCfonb120.setNewAmount(beanCfonb120NewAmount);
 	}
 
-	private BeanCfonb120Movement generateMovements() {
+	private static BeanCfonb120Movement generateMovements() {
 		BeanCfonb120Movement beanCfonb120Movement = new BeanCfonb120Movement();
 		beanCfonb120Movement.setTypeEnregistrementCode("04");
 		beanCfonb120Movement.setAdditionals(generateAdditionals());
 		return beanCfonb120Movement;
 	}
 
-	private List<BeanCfonb120Additional> generateAdditionals() {
+	private static List<BeanCfonb120Additional> generateAdditionals() {
 		List<BeanCfonb120Additional> beanCfonb120Additionals = new ArrayList<BeanCfonb120Additional>();
 		BeanCfonb120Additional beanCfonb120Additional = new BeanCfonb120Additional();
 		beanCfonb120Additional.setTypeEnregistrementCode("05");
@@ -51,7 +52,7 @@ public class Cfonb120Test extends TestCase {
 	}
 
 	@Test
-	private void cfonb120Test() {
+	public void testCfonb120() {
 		assertNotNull(beanCfonb120);
 		assertNotNull(beanCfonb120.getMovements());
 		assertNotNull(beanCfonb120.getNewAmount());
@@ -59,32 +60,32 @@ public class Cfonb120Test extends TestCase {
 	}
 	
 	@Test
-	private void MovementTest() {
+	public void testMovement() {
 		assertTrue(!beanCfonb120.getMovements().isEmpty());
 		List<BeanCfonb120Movement> beanCfonb120Movements =beanCfonb120.getMovements();
 		for (BeanCfonb120Movement beanCfonb120Movement : beanCfonb120Movements) {
-			assertEquals(beanCfonb120Movement.getTypeEnregistrementCode(), "04");
+			assertEquals("04", beanCfonb120Movement.getTypeEnregistrementCode());
 		}
 	}
 	
 	@Test
-	private void AdditionalTest() {
+	public void testAdditional() {
 		List<BeanCfonb120Movement> beanCfonb120Movements =beanCfonb120.getMovements();
 		for (BeanCfonb120Movement beanCfonb120Movement : beanCfonb120Movements) {
 			List<BeanCfonb120Additional> beanCfonb120Additionals = beanCfonb120Movement.getAdditionals();
 			for (BeanCfonb120Additional beanCfonb120Additional : beanCfonb120Additionals) {
-				assertEquals(beanCfonb120Additional.getTypeEnregistrementCode(), "05");
+				assertEquals("05", beanCfonb120Additional.getTypeEnregistrementCode());
 			}
 		}
 	}
 	
 	@Test
-	private void oldAmountTest() {
-		assertEquals(beanCfonb120.getOldAmount().getTypeEnregistrementCode(), "01");
+	public void testOldAmount() {
+		assertEquals("01", beanCfonb120.getOldAmount().getTypeEnregistrementCode());
 	}
 	
 	@Test
-	private void newAmountTest() {
-		assertEquals(beanCfonb120.getOldAmount().getTypeEnregistrementCode(), "07");
+	public void testNewAmount() {
+		assertEquals("07", beanCfonb120.getNewAmount().getTypeEnregistrementCode());
 	}
 }
