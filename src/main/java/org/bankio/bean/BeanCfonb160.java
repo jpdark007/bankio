b00e0d376c54b3e547dec4fa6d90296c34cf32ad
jpdark007 <jpdark007@users.noreply.github.com>
2016-02-24 17:06:42 +0100

    Refactor CFONB160

--- src/main/java/org/bankio/bean/BeanCfonb160.java
+++ src/main/java/org/bankio/bean/BeanCfonb160.java
@@ -1,19 +1,18 @@
 package main.java.org.bankio.bean;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashMap;
 
 public class BeanCfonb160{
 	
     private BeanCfonb160Sender sender;
-    private List<BeanCfonb160Recipient> recipients = new ArrayList<BeanCfonb160Recipient>();
+    private HashMap<Integer, BeanCfonb160Recipient> recipients = new HashMap<Integer, BeanCfonb160Recipient>();
     private BeanCfonb160Total total;
 
     public void setSender(BeanCfonb160Sender sender) {
         this.sender = sender;
     }
 
-    public void setRecipients(List<BeanCfonb160Recipient> recipients) {
+    public void setRecipients(HashMap<Integer, BeanCfonb160Recipient> recipients) {
         this.recipients = recipients;
     }
 
@@ -25,11 +24,19 @@ public class BeanCfonb160{
         return sender;
     }
 
-    public List<BeanCfonb160Recipient> getRecipients() {
+    public HashMap<Integer, BeanCfonb160Recipient> getRecipients() {
         return recipients;
     }
 
     public BeanCfonb160Total getTotal() {
         return total;
     }
+    
+	public void addRecipient(int hash, BeanCfonb160Recipient recipient) {
+		this.recipients.put(hash, recipient);
+	}
+	
+	public void addAdditionnalToRecipient(int hash, BeanCfonb160RecipientAdditional beanCfonb160RecipientAdditional) {
+		this.recipients.get(hash).setRecipientAdditional(beanCfonb160RecipientAdditional);
+	}
 }
--- src/main/java/org/bankio/bean/BeanCfonb160Recipient.java
+++ src/main/java/org/bankio/bean/BeanCfonb160Recipient.java
@@ -34,7 +34,123 @@ public class BeanCfonb160Recipient {
 	
 	private BeanCfonb160RecipientAdditional recipientAdditional;
     
-    public String getReserve1() {
+    @Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((balance == null) ? 0 : balance.hashCode());
+		result = prime * result + ((codeEnregistrement == null) ? 0 : codeEnregistrement.hashCode());
+		result = prime * result + ((codeOperation == null) ? 0 : codeOperation.hashCode());
+		result = prime * result
+				+ ((destinataireCodeEtablissement == null) ? 0 : destinataireCodeEtablissement.hashCode());
+		result = prime * result + ((destinataireCodeGuichet == null) ? 0 : destinataireCodeGuichet.hashCode());
+		result = prime * result + ((destinataireNumeroCompte == null) ? 0 : destinataireNumeroCompte.hashCode());
+		result = prime * result + ((libelle == null) ? 0 : libelle.hashCode());
+		result = prime * result + ((libelleDomiciliation == null) ? 0 : libelleDomiciliation.hashCode());
+		result = prime * result + ((montantVirement == null) ? 0 : montantVirement.hashCode());
+		result = prime * result + ((nomDestinataire == null) ? 0 : nomDestinataire.hashCode());
+		result = prime * result + ((numeroEmetteur == null) ? 0 : numeroEmetteur.hashCode());
+		result = prime * result + ((recipient == null) ? 0 : recipient.hashCode());
+		result = prime * result + ((recipientAdditional == null) ? 0 : recipientAdditional.hashCode());
+		result = prime * result + ((referenceOperation == null) ? 0 : referenceOperation.hashCode());
+		result = prime * result + ((reserve1 == null) ? 0 : reserve1.hashCode());
+		result = prime * result + ((reserve2 == null) ? 0 : reserve2.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		BeanCfonb160Recipient other = (BeanCfonb160Recipient) obj;
+		if (balance == null) {
+			if (other.balance != null)
+				return false;
+		} else if (!balance.equals(other.balance))
+			return false;
+		if (codeEnregistrement == null) {
+			if (other.codeEnregistrement != null)
+				return false;
+		} else if (!codeEnregistrement.equals(other.codeEnregistrement))
+			return false;
+		if (codeOperation == null) {
+			if (other.codeOperation != null)
+				return false;
+		} else if (!codeOperation.equals(other.codeOperation))
+			return false;
+		if (destinataireCodeEtablissement == null) {
+			if (other.destinataireCodeEtablissement != null)
+				return false;
+		} else if (!destinataireCodeEtablissement.equals(other.destinataireCodeEtablissement))
+			return false;
+		if (destinataireCodeGuichet == null) {
+			if (other.destinataireCodeGuichet != null)
+				return false;
+		} else if (!destinataireCodeGuichet.equals(other.destinataireCodeGuichet))
+			return false;
+		if (destinataireNumeroCompte == null) {
+			if (other.destinataireNumeroCompte != null)
+				return false;
+		} else if (!destinataireNumeroCompte.equals(other.destinataireNumeroCompte))
+			return false;
+		if (libelle == null) {
+			if (other.libelle != null)
+				return false;
+		} else if (!libelle.equals(other.libelle))
+			return false;
+		if (libelleDomiciliation == null) {
+			if (other.libelleDomiciliation != null)
+				return false;
+		} else if (!libelleDomiciliation.equals(other.libelleDomiciliation))
+			return false;
+		if (montantVirement == null) {
+			if (other.montantVirement != null)
+				return false;
+		} else if (!montantVirement.equals(other.montantVirement))
+			return false;
+		if (nomDestinataire == null) {
+			if (other.nomDestinataire != null)
+				return false;
+		} else if (!nomDestinataire.equals(other.nomDestinataire))
+			return false;
+		if (numeroEmetteur == null) {
+			if (other.numeroEmetteur != null)
+				return false;
+		} else if (!numeroEmetteur.equals(other.numeroEmetteur))
+			return false;
+		if (recipient == null) {
+			if (other.recipient != null)
+				return false;
+		} else if (!recipient.equals(other.recipient))
+			return false;
+		if (recipientAdditional == null) {
+			if (other.recipientAdditional != null)
+				return false;
+		} else if (!recipientAdditional.equals(other.recipientAdditional))
+			return false;
+		if (referenceOperation == null) {
+			if (other.referenceOperation != null)
+				return false;
+		} else if (!referenceOperation.equals(other.referenceOperation))
+			return false;
+		if (reserve1 == null) {
+			if (other.reserve1 != null)
+				return false;
+		} else if (!reserve1.equals(other.reserve1))
+			return false;
+		if (reserve2 == null) {
+			if (other.reserve2 != null)
+				return false;
+		} else if (!reserve2.equals(other.reserve2))
+			return false;
+		return true;
+	}
+
+	public String getReserve1() {
 		return reserve1;
 	}
 
